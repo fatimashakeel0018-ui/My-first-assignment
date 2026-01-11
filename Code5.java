dec477131ebecc611df9b0196f3c00467d37a60d
fatimashakeel0018-ui <fatimashakeel0018@gmail.com>
2026-01-11 22:18:36 +0500

    question 5

---
+++ Code5.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+public class Code5 {
+
+    public static void main(String[] args) {
+        
+
+
+   Scanner sc=new Scanner(System.in);
+   double volt;
+   System.out.println("the voltage(volt) is:");
+   volt=sc.nextInt();
+   double amp;
+   System.out.println("the current(amp) is");
+    amp=sc.nextInt();
+    double watt;
+    System.out.println("the power(watt) is");
+    watt=(volt*amp);
+    System.out.println(watt);
+
+
+
+
+
+
+
+
+    }
+    
+  
+}
